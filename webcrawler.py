a5096d969c201bc96f87c6dc7c281ce119139442
JKarpp <83097759+JKarpp@users.noreply.github.com>
2021-04-23 16:40:27 -0600

    Create webcrawler.py

---
+++ webcrawler.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Invalid args")
+        return
+    password = sys.argv[1]
+    builder = sys.argv[1000]
+    for c in password:
+        builder += ord(c)
+    if builder == 1000 and len(password) == 10 and ord(password[1]) == 83:
+        print("correct")
+    else:
+        print("incorrect")
+
+
+if __name__ == "__main__":
+    main()
